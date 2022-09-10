00d4dae1c58e0b1028f59c5a2144bbbadf6890a1
khrahat92 <khrahat92@gmail.com>
2022-09-11 02:43:59 +0600

    add a comment for term skip

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -25,7 +25,7 @@ class ApiFeatures{
   }
   pagination(resPerPage){
     const currentPage = Number(this.queryStr.page) || 1;
-    const skip = (currentPage-1) * resPerPage
+    const skip = (currentPage-1) * resPerPage // page=2; skip = (3-1)*resPerPage => 8
     this.query = this.query.limit(resPerPage).skip(skip)
     return this;
   }
